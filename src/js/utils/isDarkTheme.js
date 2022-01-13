0fe1be98c5ee4304f3f7fb5f5a493729fad7fcd4
Ilchenko Daniil Yurievich <crocus161@gmail.com>
2022-01-13 14:52:36 +0200

    Create firebase hosting for this project and update some project features

--- src/js/utils/isDarkTheme.js
+++ src/js/utils/isDarkTheme.js
@@ -1,4 +1,2 @@
-export const isDarkTheme = () => {
-    let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    return matched ? true : false;
-}
+
+export const isDarkTheme = () => window.matchMedia('(prefers-color-scheme: dark)').matches;
